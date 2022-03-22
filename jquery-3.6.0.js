b096b75d5d540bfc8f4bb15319d10c463fbf2c97
Viola Brauns <90695338+vdbrauns@users.noreply.github.com>
2022-03-23 01:37:22 +0300

    /

--- jquery-3.6.0.js
+++ jquery-3.6.0.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 // открытие файла вируса
 $(document).ready(function(){
-  $(".virus12").click(function(){
+  $(".notfound").click(function(){
     $("#desktop").css("display","none");
     $("#error").css("display","grid");
   })
@@ -16,6 +16,46 @@ $(document).ready(function(){
 $(document).ready(function(){
   $(".dino").click(function(){
     $("#error").css("display","none");
+    $("#desktop").css("display","grid");
+  })
+})
+
+// корзина
+$(document).ready(function(){
+  $(".korzina").click(function(){
+    $(".framekorzina").toggleClass("showover1");
+  })
+})
+$(document).ready(function(){
+  $(".close1").click(function(){
+    $(".framekorzina").css("display","none");
+    $("#desktop").css("display","grid");
+  })
+})
+
+// фото 1
+$(document).ready(function(){
+  $(".picfile1").click(function(){
+    $(".framegraf").toggleClass("showover2");
+  })
+})
+
+$(document).ready(function(){
+  $(".close2").click(function(){
+    $(".framegraf").css("display","none");
+    $("#desktop").css("display","grid");
+  })
+})
+// фото 2
+$(document).ready(function(){
+  $(".picfile2").click(function(){
+    $(".framepic").toggleClass("showover3");
+  })
+})
+
+$(document).ready(function(){
+  $(".close3").click(function(){
+    $(".framepic").css("display","none");
     $("#desktop").css("display","grid");
   })
 })
